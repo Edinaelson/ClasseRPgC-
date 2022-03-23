a167d0a901397cbec323a8d0fa5907edc9ccf285
edinaelson <sedinaelson@gmail.com>
2022-03-23 11:39:21 -0300

    Atualizando classes

---
+++ RpgBattle/Humano.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RpgBattle
+{
+    public class Humano
+    {
+        public String Nome { get; set; }
+        public String Forca { get; set; }
+        public String HabilidadeEspecial { get; set; }
+        public String Raca { get; set; }
+
+        public Humano(String nome, String forca, String habilidadeEspecial, String raca)
+        {
+            Nome = nome;
+            Forca = forca;
+            HabilidadeEspecial = habilidadeEspecial;
+            Raca = raca;
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Forca: " + Forca);
+            Console.WriteLine("Habilidade: " + HabilidadeEspecial);
+            Console.WriteLine("Raca: " + Raca);
+        }
+    }
+    
+    
+}
---
+++ RpgBattle/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RpgBattle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Humano edi = new Humano("edi", "100", "invisivel", "humano");
+            
+        }
+    }
+}
